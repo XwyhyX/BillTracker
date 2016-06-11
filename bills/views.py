fabe91221bb38e71f0befa7f24ce784ce8d0d0ca
John Rei Enriquez <johnrei.enriquez@gmail.com>
2016-06-11 09:22:41 +0800

    proper detail page loading

--- bills/views.py
+++ bills/views.py
@@ -5,12 +5,10 @@ from .models import Bills
 def index(request):
 	object_list = Bills.objects.all()
 	context = {"object_list":object_list}
-	print object_list
 	return render(request, 'bills/index.html',context)
 def pay(request):
 	object_list = Bills.objects.all().filter(status="UD")
 	context = {"object_list":object_list}
-	print object_list
 	return render(request, 'bills/pay.html',context)
 def history(request):
 	object_list = Bills.objects.all()
